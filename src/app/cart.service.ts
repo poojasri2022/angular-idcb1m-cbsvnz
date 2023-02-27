10be83e1c23cadc01529a378325f331f61203697
poojasri2022 <119938326+poojasri2022@users.noreply.github.com>
2023-02-27 09:44:28 +0530

    Initial commit

---
+++ src/app/cart.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Product, products } from './products';
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  items: Product[]=[];
+
+  constructor() { }
+
+  addItems(product:Product)
+  {
+    this.items.push(product);
+    console.log(this.items);
+  }
+}
